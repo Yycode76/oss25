128ed3399bf0007da91301cfec0fd3f1acfd1e0f
Yycode76 <rladbsky@naver.com>
2025-04-01 11:21:47 +0900

    Title: 두줄 출력, Time: 7ms, Memory: 0MB, Status: Passed - Codetree

--- 250401/두줄 출력/print-two-sentences-introduce.cpp
+++ 250401/두줄 출력/print-two-sentences-introduce.cpp
@@ -3,6 +3,6 @@ using namespace std;
 
 int main() {
     // Please write your code here.
-    cout << "Hello students!" << endl << "Welcome to LeebresCode!";
+    cout << "Hello students!" << endl << "Welcome to LeebrosCode!";
     return 0;
 }
