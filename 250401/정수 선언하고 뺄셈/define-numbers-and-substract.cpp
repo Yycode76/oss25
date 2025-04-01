29ea094927c583d4a1278416a5683b5fed84eb13
Yycode76 <rladbsky@naver.com>
2025-04-01 11:37:46 +0900

    Title: 정수 선언하고 뺄셈, Time: 7ms, Memory: 0MB, Status: Wrong Answer - Codetree

---
+++ 250401/정수 선언하고 뺄셈/define-numbers-and-substract.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int n_1 = 97;
+    int n_2 = 13;
+
+    cout << n_1 << " - " << n_2 << n_1 - n_2;
+    return 0;
+}
