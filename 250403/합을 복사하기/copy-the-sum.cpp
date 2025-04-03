b8b9675bf55367b0c0c2bba621d6f827919694d4
Yycode76 <rladbsky@naver.com>
2025-04-03 19:02:48 +0900

    Title: 합을 복사하기, Time: -1ms, Memory: -1MB, Status: Compile Error - Codetree

---
+++ 250403/합을 복사하기/copy-the-sum.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    //변수 선언
+    int a = 1, b = 2, c = 3;
+
+    //변수 저장
+    a = b = c = a+b+c;
+
+    cout << a " " << b << " " << c;
+    return 0;
+}
