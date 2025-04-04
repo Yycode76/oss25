c718bf85c7459d476e7382d1a20f7da352c8896b
Yycode76 <rladbsky@naver.com>
2025-04-04 14:29:23 +0900

    Title: 전화번호 바꾸기, Time: 6ms, Memory: 0MB, Status: Wrong Answer - Codetree

---
+++ 250404/전화번호 바꾸기/change-phone-number.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+    string a,b,c;
+    char dash;
+
+    cin >> a >> dash >> b >> dash >> c;
+
+    cout << a << '-' << c << '-' << b ;
+    return 0;
+}
