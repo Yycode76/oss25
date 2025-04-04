1b13c805d1594b789c8fe1fcdbc384f565c614ed
Yycode76 <rladbsky@naver.com>
2025-04-04 14:56:45 +0900

    Title: 전화번호 바꾸기, Time: 6ms, Memory: 0MB, Status: Passed - Codetree

--- 250404/전화번호 바꾸기/change-phone-number.cpp
+++ 250404/전화번호 바꾸기/change-phone-number.cpp
@@ -1,14 +1,16 @@
 #include <iostream>
 #include <string>
-
 using namespace std;
 
 int main() {
-    string a,b,c;
-    char dash;
+    string input, a, b, c;
 
-    cin >> a >> dash >> b >> dash >> c;
+    getline(cin, input);  // "010-1234-5678" 통째로 읽음
 
-    cout << a << '-' << c << '-' << b ;
+    a = input.substr(0, 3);   // "010"
+    b = input.substr(4, 4);   // "1234"
+    c = input.substr(9, 4);   // "5678"
+
+    cout << a << '-' << c << '-' << b << endl;
     return 0;
 }
