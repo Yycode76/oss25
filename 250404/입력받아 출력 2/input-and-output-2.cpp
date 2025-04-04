212658b91a5658625ec33fb499386e3db1f1e41b
Yycode76 <rladbsky@naver.com>
2025-04-04 14:08:03 +0900

    Title: 입력받아 출력 2, Time: 6ms, Memory: 0MB, Status: Passed - Codetree

--- 250404/입력받아 출력 2/input-and-output-2.cpp
+++ 250404/입력받아 출력 2/input-and-output-2.cpp
@@ -1,12 +1,14 @@
 #include <iostream>
-#include <string>
 using namespace std;
 
 int main() {
-    string a,b;
+    int a,b;
     char c;
 
     cin >> a;
-    cin.get
+    cin.get();
+    cin >> b;
+
+    cout << a << b;
     return 0;
 }
