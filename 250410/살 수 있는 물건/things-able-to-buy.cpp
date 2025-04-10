9a41678ece9d8bd5bc926b9cf24ebf5d6b7a7fb8
Yycode76 <rladbsky@naver.com>
2025-04-10 21:00:28 +0900

    Title: 살 수 있는 물건, Time: 6ms, Memory: 0MB, Status: Wrong Answer - Codetree

---
+++ 250410/살 수 있는 물건/things-able-to-buy.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int n;
+    cin >> n;
+
+    if( n >= 3000 )
+        cout << "book";
+    else
+        cout << "mask";
+    return 0;
+}
