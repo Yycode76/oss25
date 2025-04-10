57b398d8c005ca31bd6e37912296fbbd1cf62b6f
Yycode76 <rladbsky@naver.com>
2025-04-10 20:15:22 +0900

    Title: 음수 구별하기, Time: 6ms, Memory: 0MB, Status: Passed - Codetree

--- 250410/음수 구별하기/separate-negative-number.cpp
+++ 250410/음수 구별하기/separate-negative-number.cpp
@@ -6,6 +6,9 @@ int main() {
     int n;
     cin >> n;
 
-    if(n < 0) cout << n << endl << "minus";
+    if(n < 0) {
+        cout << n << endl << "minus";
+    }
+    else cout << n;
     return 0;
 }
