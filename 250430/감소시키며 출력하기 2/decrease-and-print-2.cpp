a0a84fc57614e038be9ec679c8d54f719324680f
Yycode76 <rladbsky@naver.com>
2025-04-30 15:28:23 +0900

    Title: 감소시키며 출력하기 2, Time: -1ms, Memory: -1MB, Status: Compile Error - Codetree

---
+++ 250430/감소시키며 출력하기 2/decrease-and-print-2.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int i = 26
+    while(i > 9){
+        cout << i << " ";
+        i--;
+    }
+    return 0;
+}
