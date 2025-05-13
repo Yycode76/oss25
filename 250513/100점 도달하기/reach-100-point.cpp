b6845cdbfdc885f6738a6e7d1d7425ab0a18e5bf
Yycode76 <rladbsky@naver.com>
2025-05-13 15:18:15 +0900

    Title: 100점 도달하기, Time: -1ms, Memory: -1MB, Status: Compile Error - Codetree

---
+++ 250513/100점 도달하기/reach-100-point.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    #include <iostream>
+
+using namespace std;
+
+int main() {
+    // 변수 선언
+    int n;
+
+	// 입력
+	cin >> n;
+    
+    // 출력
+	for(int i = n; i <= 100; i++) {
+		if(i >= 90)
+			cout << "A" << " ";
+		else if(i >= 80)
+			cout << "B" << " ";
+		else if(i >= 70)
+			cout << "C" << " ";
+		else if(i >= 60)
+			cout << "D" << " ";
+		else
+			cout << "F" << " ";
+	}
+
+    return 0;
+}
+
+    return 0;
+}
