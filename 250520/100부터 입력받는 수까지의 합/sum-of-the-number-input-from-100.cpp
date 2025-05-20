599482237a0d8d521c3eb72dd2350cc55f99d51c
Yycode76 <rladbsky@naver.com>
2025-05-20 14:29:10 +0900

    Title: 100부터 입력받는 수까지의 합, Time: 6ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 250520/100부터 입력받는 수까지의 합/sum-of-the-number-input-from-100.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int n, sum=0;
+    cin >> n;
+
+    for ( int i=n;i<=100;i++){
+        sum = sum+i;
+    }
+    cout << sum;
+    return 0;
+}
