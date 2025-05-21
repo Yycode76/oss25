6e66c22cbf0f3c14b39c119c70ef6f0324ea7794
Yycode76 <rladbsky@naver.com>
2025-05-21 10:55:18 +0900

    Title: 입력받은 수의 합과 평균, Time: 7ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 250521/입력받은 수의 합과 평균/sum-and-average-of-the-inputs.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n,a;
+    int sum = 0;
+    cin >> n;
+     for( int i=0;i<n;i++ ){
+        cin >> a;
+        sum += a;
+     }
+     double avg= (double)sum/n;
+     cout << fixed;
+     cout.precision(1);
+     cout << sum << " " << avg;
+    return 0;
+}
