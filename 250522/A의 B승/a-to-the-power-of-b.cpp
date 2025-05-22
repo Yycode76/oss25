e3fd08ce40e08c00fd7bf5256c54b04af5b21353
Yycode76 <rladbsky@naver.com>
2025-05-22 14:29:58 +0900

    Title: A의 B승, Time: 6ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 250522/A의 B승/a-to-the-power-of-b.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+     int a,b;
+     int prod = 1;
+
+     cin >> a >> b;
+
+     for(  int i = 0; i<b;i++){
+        prod *= a;
+     }
+     cout << prod;
+    return 0;
+}
