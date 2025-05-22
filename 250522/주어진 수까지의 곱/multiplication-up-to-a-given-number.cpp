51165f698959dcb18ba43f6b4a12960e2d12ffc7
Yycode76 <rladbsky@naver.com>
2025-05-22 14:25:35 +0900

    Title: 주어진 수까지의 곱, Time: 6ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 250522/주어진 수까지의 곱/multiplication-up-to-a-given-number.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a,b;
+    int prod = 1;
+
+    cin >> a >> b;
+
+    for( int i = a; i <= b; i++){
+        prod *= i;
+    }
+    cout << prod;
+    return 0;
+}
