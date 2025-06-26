f6d1a6fc2b9ad4c350066e4c0f7751f963437fe7
Yycode76 <rladbsky@naver.com>
2025-06-27 00:47:08 +0900

    Title: 3N + 1 수열, Time: 8ms, Memory: 0MB, Status: Wrong Answer - Codetree

---
+++ 250627/3N + 1 수열/3n-plus-1-sequence.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    int cnt = 0;
+
+    while(true){
+        cin >> n;
+
+        if( n % 2 == 0){
+            n = n/2;
+        }
+
+        else{
+            n = n*3 + 1;
+        }
+
+        cnt++;
+        if(n==1)
+        break;
+       
+    }
+    cout << cnt;
+    return 0;
+}
